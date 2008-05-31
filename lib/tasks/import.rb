4e73797cc3c7100e7aea84775aa2d3235088b7f8
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-05-31 21:52:39 +0000

    Make adding one big transaction
    git-svn-id: svn://p51.austin.ibm.com/condor/trunk@221 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

--- lib/tasks/import.rb
+++ lib/tasks/import.rb
@@ -1,35 +1,37 @@
 #!/usr/bin/env script/runner
 
 File.open(ARGV[0]) do |file|
-  file.each_line do |line|
-    fields = line.split(/\|/)
-
-    puts file.lineno if (d, m = file.lineno.divmod(1000)) && m == 0
-    
-    ptf = Ptf.find_or_create_by_name fields[0]
-    apar = Apar.find_or_create_by_name fields[1]
-    defect = Defect.find_or_create_by_name fields[2]
-    base = Base.find_or_create_by_name fields[3]
-    lpp = base.lpps.find_or_create_by_name fields[4]
-
-    family = Family.find_or_create_by_name fields[6]
-    releases = fields[5].split(/ /).map do |release|
-      family.releases.find_or_create_by_name release
+  Apar.transaction do 
+    file.each_line do |line|
+      fields = line.split(/\|/)
+      
+      puts file.lineno if (d, m = file.lineno.divmod(1000)) && m == 0
+      
+      ptf = Ptf.find_or_create_by_name fields[0]
+      apar = Apar.find_or_create_by_name fields[1]
+      defect = Defect.find_or_create_by_name fields[2]
+      base = Base.find_or_create_by_name fields[3]
+      lpp = base.lpps.find_or_create_by_name fields[4]
+      
+      family = Family.find_or_create_by_name fields[6]
+      releases = fields[5].split(/ /).map do |release|
+        family.releases.find_or_create_by_name release
+      end
+      
+      if apar.abstract.nil?
+        apar.abstract = fields[7]
+        apar.save
+      end
+      
+      fileset = lpp.filesets.find_or_create_by_vrmf fields[8]
+      
+      AparDefectMap.find_or_create_by_apar_id_and_defect_id(apar.id, defect.id)
+      AparPtfMap.find_or_create_by_apar_id_and_ptf_id(apar.id, ptf.id)
+      releases.each do |release|
+        DefectReleaseMap.find_or_create_by_defect_id_and_release_id(defect.id,
+                                                                    release.id)
+      end
+      FilesetPtfMap.find_or_create_by_fileset_id_and_ptf_id(fileset.id, ptf.id)
     end
-
-    if apar.abstract.nil?
-      apar.abstract = fields[7]
-      apar.save
-    end
-
-    fileset = lpp.filesets.find_or_create_by_vrmf fields[8]
-
-    AparDefectMap.find_or_create_by_apar_id_and_defect_id(apar.id, defect.id)
-    AparPtfMap.find_or_create_by_apar_id_and_ptf_id(apar.id, ptf.id)
-    releases.each do |release|
-      DefectReleaseMap.find_or_create_by_defect_id_and_release_id(defect.id,
-                                                                  release.id)
-    end
-    FilesetPtfMap.find_or_create_by_fileset_id_and_ptf_id(fileset.id, ptf.id)
   end
 end
