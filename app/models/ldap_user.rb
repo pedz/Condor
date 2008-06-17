52425d9a9127421986e5f0e10d71105f6746ca7e
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-06-17 12:53:20 +0000

    add Ldap to project.  Having issues though
    git-svn-id: svn://p51.austin.ibm.com/condor/trunk@238 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

---
+++ app/models/ldap_user.rb
@@ -0,0 +1,20 @@
+class LdapUser < ActiveLdap::Base
+  ldap_mapping :dn_attribute => "uid", :prefix => "c=us,ou=bluepages,o=ibm.com"
+  # ldap_mapping :dn_attribute => "uid", :prefix => "ou=bluepages"
+
+  # belongs_to :manager, :class => 'LdapUser', :foreign_key => 'manager', :primary_key => 'dn'
+  # belongs_to :manager, :class => 'LdapUser'
+  has_many   :manages, :class => 'LdapUser', :foreign_key => 'dn', :primary_key => 'manager'
+ 
+  def self.authenticate_from_email(email, password)
+    return nil unless (u = find(:attribute => "mail", :value => email))
+    begin
+      dn = u.dn.gsub(/\+/, "\\\\+")
+      logger.debug("authenticate_from_email #{email} => #{dn}")
+      u.connection.rebind(:allow_anonymous => false, :password => password, :bind_dn => dn)
+    rescue
+      logger.debug("authenticate_from_email denied")
+      nil
+    end
+  end
+end
