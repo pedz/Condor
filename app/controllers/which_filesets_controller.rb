919d70019e3cdb560543c06da456f00bd7fc18fd
pedzan <pedzan@c4f43c74-3acd-11dc-88bc-9584e4a9cacb>
2008-08-20 23:13:05 +0000

    changes to find files after the 32 or 64 additions have been removed
    git-svn-id: svn://p51.austin.ibm.com/condor/trunk@317 c4f43c74-3acd-11dc-88bc-9584e4a9cacb

--- app/controllers/which_filesets_controller.rb
+++ app/controllers/which_filesets_controller.rb
@@ -6,15 +6,32 @@ class WhichFilesetsController < ApplicationController
   def show
     path = params[:path].join('/')
     logger.debug("Accepts = #{request.accepts.inspect}")
-    @files = AixFile.find(:all,
-                          :conditions => ("basename(path) = basename('#{path}') AND " +
-                                          "path LIKE '%#{path}'"),
-                          :order => "path",
-                          :include => [ { :filesets => :lpp }])
+    @files = do_find(path)
+    if @files.empty?
+      if (md = /(.*)(_?64$|_?32$)/.match(path))
+        path = md[1]
+        @files = do_find(path)
+      elsif (md = /^(.*)(32|64)(\..*)$/.match(path))
+        path = md[1] + md[3]
+        @files = do_find(path)
+      end
+    end
 
     respond_to do |format|
       format.html { render :action => "show" }
-      format.xml  { render :xml => @files.to_xml(:include => { :filesets => { :include => :lpp }}) }
+      format.xml  {
+        render :xml => @files.to_xml(:include => { :filesets => { :include => :lpp }})
+      }
     end
+  end
+  
+  private
+  
+  def do_find(path)
+    AixFile.find(:all,
+                 :conditions => ("basename(path) = basename('#{path}') AND " +
+                                 "path LIKE '%#{path}'"),
+                 :order => "path",
+                 :include => [ { :filesets => :lpp }])
   end
 end
