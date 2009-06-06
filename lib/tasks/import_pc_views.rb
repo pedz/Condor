616431d5c580d5a45f7a5064ac9e09de8184fabe
Perry Smith (IBM) <pedzan@newtoy.easesoftware.com>
2009-06-06 18:31:08 -0500

    New version of import_pc_views to use the new mapping scheme

--- lib/tasks/import_pc_views.rb
+++ lib/tasks/import_pc_views.rb
@@ -1,46 +1,43 @@
 #!/usr/bin/env ruby
 
+# Sample input:
+#
+# 315731|rayc|aix|42G|APPROVED|NO_REQS_NEEDED|NO_NEEDED|
+# 302632|frankzhu|aix|42G|APPROVED|NO_REQS_NEEDED|NEEDS_ONE|
+# 220324|frankzhu|aix|42G|OPEN|UNKNOWN|NOT_SPECIFIED|
+
+
 require File.dirname(__FILE__) + '/../../script/runner'
 
 GOOD_FAMILY = Regexp.new("aix")
-GOOD_PRODUCT = Regexp.new("^(5[234].|6..)$")
+GOOD_VERSION = Regexp.new("^(5[234].|6..)$")
 
-nby_ptf = Ptf.find(:first, :conditions => { :name => "Not Built Yet" })
-nby_version = Version.find(:first, :conditions => { :name => "Not Built Yet" })
-
-def create_map(maps, hash, ptf)
-  unless maps.detect { |m| m.ptf && m.ptf.id == ptf.id }
-    hash[:ptf_id] = ptf.id
-    hash[:version_id] = nby_version.id
-    AparDefectPtfVersionMap.create(hash)
-  end
-end
-
-File.open(ARGV[0]) do |file|
-  Apar.transaction do
-    file.each_line do |line|
-      line.chomp!
-      defect, owner, family, product, state, apar_state, apar, ptfs = line.split('|')
-      next unless GOOD_FAMILY.match(family)
-      next unless GOOD_PRODUCT.match(product)
-      defect = Defect.find_or_create_by_name defect
-      apar = Apar.find_or_create_by_name apar
-
-      hash = {
-        :apar_id => apar.id,
-        :defect_id => defect.id
-      }
-
-      maps = AparDefectPtfVersionMap.find(:all, :conditions => hash)
-
-      if ptfs.nil?
-        create_map(maps, hash, nby_ptf)
-      else
-        ptfs.split(', ').each do |ptf|
-          ptf = Ptf.find_or_create_by_name ptf
-          create_map(maps, hash, ptf)
+begin
+  File.open(ARGV[0]) do |file|
+    Apar.transaction do
+      file.each_line do |line|
+        line.chomp!
+        defect, owner, family, version, state, apar_state, apar, ptfs = line.split('|')
+        next unless GOOD_FAMILY.match(family)
+        next unless GOOD_VERSION.match(version)
+        defect = Defect.find_or_create_by_name defect
+        apar = Apar.find_or_create_by_name apar
+        version = Version.find_or_create_by_name version
+        
+        adv_hash = {
+          :apar_id => apar.id,
+          :defect_id => defect.id,
+          :version_id => version.id
+        }
+        adv = AparDefectVersionMap.find(:first, :conditions => adv_hash)
+        unless adv
+          adv = AparDefectVersionMap.create(adv_hash)
         end
       end
     end
   end
+rescue => e
+  puts e.message
+  puts e.backtrace
+  exit 1
 end
