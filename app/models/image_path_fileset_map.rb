57856efcf0ce0be5d29743079aa792487f42e0d6
Perry Smith (IBM) <pedzan@NewToy.austin.ibm.com>
2009-06-15 18:51:27 -0500

    Delete map models that no longer exist.

--- app/models/image_path_fileset_map.rb
+++
@@ -1,4 +0,0 @@
-class ImagePathFilesetMap < ActiveRecord::Base
-  belongs_to :image_path
-  belongs_to :fileset
-end
--- app/models/image_path_package_map.rb
+++
@@ -1,4 +0,0 @@
-class ImagePathPackageMap < ActiveRecord::Base
-  belongs_to :image_path
-  belongs_to :package
-end
