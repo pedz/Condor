44a902d452ffb6064259b15deea8c7648b2ebcde
Perry Smith (IBM) <pedzan@newtoy.easesoftware.com>
2009-07-17 15:40:02 -0500

    first attempt to put xml back into swinfos controller.

--- app/controllers/swinfos_controller.rb
+++ app/controllers/swinfos_controller.rb
@@ -1,63 +1,64 @@
 class SwinfosController < ApplicationController
+  ATTR_LIST = [
+               :abstract,
+               :apar,
+               :defect,
+               :lpp,
+               :ptf,
+               :release,
+               :service_pack,
+               :version,
+               :vrmf
+              ]
+
+  METH_LIST = [
+               :apar_draft_apar_path,
+               :apar_draft_defect_path,
+               :changes_path,
+               :defect_path,
+               :fileset_path,
+               :swinfos_apar_path,
+               :swinfos_defect_path,
+               :swinfos_fileset_path,
+               :swinfos_lpp_path,
+               :swinfos_ptf_path
+              ]
   def show
     logger.debug("Accepts = #{request.accepts.inspect}")
     @param = params[:item]
     respond_to do |format|
       format.html { render :action => "show", :layout => "spry" }
-      format.json {
-        item = @param
-        item_upcase = item.upcase
-        case item_upcase
-        when /^I[XYZ][0-9][0-9][0-9][0-9][0-9]$/ # APAR
-          @items = Ptfapardef.find_all_by_apar(item_upcase)
-          
-        when /^[0-9]+$/         # Defect
-          @items = Ptfapardef.find_all_by_defect(item)
-          
-
-        when /^U[0-9][0-9][0-9][0-9][0-9][0-9]$/ # PTF
-          @items = Ptfapardef.find_all_by_ptf(item_upcase)
-          
-        when /^([^ ]+) +([^ ]+)$/ # Fileset name with vrmf
-          lpp, vrmf = item.split(' ')
-          @items = Ptfapardef.find_all_by_lpp(lpp,
-                                              :conditions => [ 'vrmf LIKE ?', "#{vrmf}%"])
-
-        else                    # Just a fileset name
-          @items = Ptfapardef.find_all_by_lpp(item)
-        end
-        @items.each { |item| item.controller = self }
-        render :json => @items.to_json(:only => [
-                                                 :abstract,
-                                                 :apar,
-                                                 :defect,
-                                                 :lpp,
-                                                 :ptf,
-                                                 :release,
-                                                 :service_pack,
-                                                 :version,
-                                                 :vrmf
-                                                ],
-                                       :methods => [
-                                                    :apar_draft_apar_path,
-                                                    :apar_draft_defect_path,
-                                                    :changes_path,
-                                                    :defect_path,
-                                                    :fileset_path,
-                                                    :swinfos_apar_path,
-                                                    :swinfos_defect_path,
-                                                    :swinfos_fileset_path,
-                                                    :swinfos_lpp_path,
-                                                    :swinfos_ptf_path
-                                                   ])
-      }
+      format.json { render :json => @items.to_json(:only => ATTR_LIST,
+                                                   :methods => METH_LIST) }
+      format.xml  { render :xml => @items.to_xml(:only => ATTR_LIST,
+                                                 :methods => METH_LIST) }
     end
   end
+
+  private
+
+  def find_items
+    item = @param
+    item_upcase = item.upcase
+    case item_upcase
+    when /^I[XYZ][0-9][0-9][0-9][0-9][0-9]$/ # APAR
+      @items = Ptfapardef.find_all_by_apar(item_upcase)
+      
+    when /^[0-9]+$/         # Defect
+      @items = Ptfapardef.find_all_by_defect(item)
+      
+      
+    when /^U[0-9][0-9][0-9][0-9][0-9][0-9]$/ # PTF
+      @items = Ptfapardef.find_all_by_ptf(item_upcase)
+      
+    when /^([^ ]+) +([^ ]+)$/ # Fileset name with vrmf
+      lpp, vrmf = item.split(' ')
+      @items = Ptfapardef.find_all_by_lpp(lpp,
+                                          :conditions => [ 'vrmf LIKE ?', "#{vrmf}%"])
+      
+    else                    # Just a fileset name
+      @items = Ptfapardef.find_all_by_lpp(item)
+    end
+    @items.each { |item| item.controller = self }
+  end
 end
-
-
-#       format.html { render :action => "show" }
-#       format.xml  { render :xml => @items.to_xml }
-#       format.json {
-#         logger.debug("json: #{@items.inspect}")
-#       }
