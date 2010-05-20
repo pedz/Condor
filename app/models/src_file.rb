e0372fa4e124673aa0ab26983120e5ff0e97dcc2
Perry Smith (IBM) <pedzan@newtoy.austin.ibm.com>
2010-05-19 19:46:12 -0500

    Debugging for popen failure

--- app/models/src_file.rb
+++ app/models/src_file.rb
@@ -1,6 +1,6 @@
 class SrcFile < SCM::CMVC
   def self.find(options, user)
-    logger.debug("hi")
+    logger.info("hi from SrcFile")
     raise SCM::LoginRequired.new unless (user.cmvc && !user.cmvc.blank?)
     string = "/usr/local/cmvc/bin/File \
                 -extract #{options[:path]} \
@@ -11,6 +11,7 @@ class SrcFile < SCM::CMVC
                 -stdout"
     cmd_result = popen(string)
     if cmd_result.exit_status != 0
+      logger.info("stderr = #{cmd_result.stderr}")
       raise SCM::PopenFailed.new(string, cmd_result.exit_status, cmd_result.stderr)
     end
     cmd_result.stdout
