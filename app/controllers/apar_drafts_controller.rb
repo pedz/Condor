c0490a9b19148d36b26901b1020000c423f39fba
Perry Smith (IBM) <pedzan@newtoy.austin.ibm.com>
2010-12-02 10:56:30 -0600

    Fixed apar drafts to go to the new site

--- app/controllers/apar_drafts_controller.rb
+++ app/controllers/apar_drafts_controller.rb
@@ -1,5 +1,5 @@
 class AparDraftsController < ApplicationController
-  APAR_DRAFT_URL = "https://reports.austin.ibm.com/cgi-bin/secure_aparmgt?search_type=%s&search_arg=%s&ACTION=draft_search"
+  APAR_DRAFT_URL = "https://amt.austin.ibm.com/cgi-bin/secure_aparmgt?search_type=%s&search_arg=%s&ACTION=draft_search"
   def show
     new_url = APAR_DRAFT_URL % [params[:search_type], params[:search_arg]]
     redirect_to new_url
