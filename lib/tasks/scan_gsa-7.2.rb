5bc1ab3321693475a0ae5a909b5251631c5315a5
Perry Smith <pedz@easesoftware.com>
2016-11-29 10:08:08 -0600

    Changed scan_gsa patterns
    Often the build group has 7100-00-03_SPGold and not 7100-00-03_SP.  I
    changed the patterns from ??Gold to *Gold to match.

--- lib/tasks/scan_gsa-7.2.rb
+++ lib/tasks/scan_gsa-7.2.rb
@@ -7,6 +7,6 @@
 
 # 7.2 base and pattern
 GSA_BASE    = Pathname.new("/gsa/ausgsa/projects/a/aix")
-GSA_PATTERN = GSA_BASE + "aix72?/7200-{??Gold,*_SP}/{update,inst}.images"
+GSA_PATTERN = GSA_BASE + "aix72?/7200-{*Gold,*_SP}/{update,inst}.images"
 
 require 'lib/tasks/scan_gsa'
